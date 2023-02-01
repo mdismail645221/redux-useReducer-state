a257ad39eab3c304e6c0fa4c4e7ab2b706144ad4
mdismail645221 <mdismail645221@gmail.com>
2023-02-01 17:07:58 +0600

    useReducer concept added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { createContext, useState } from 'react';
+
 import './App.css';
 import Child from './components/Child';
-import Perent from './components/Prerent/Perent';
 
 
-export const COUNTER_CONTEXT = createContext()
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const value = {
-    count,
-    setCount
-  }
   return (
     <div className="App">
-      <COUNTER_CONTEXT.Provider value={value}>
-        <Perent></Perent>
+
+        {/* <Perent></Perent> */}
         <Child></Child>
-      </COUNTER_CONTEXT.Provider>
     </div>
   );
 }
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react';
-import { COUNTER_CONTEXT } from '../App';
+import React, { useReducer } from 'react';
+// import { act } from 'react-dom/test-utils';
 import "../components/Child.css"
 
 const Child = () => {
 
+    const initialState = 0;
+    const reducer = (state, action) => {
+        console.log(action)
+        if(action.type === "DECREMENT"){
+            return state - action.playload;
+        }else if(action.type === "INCREMENT"){
+            return state + action.playload ;
+        }
+    }
 
-    const { count, setCount } = useContext(COUNTER_CONTEXT)
-
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <div> {/*className='h-screen bg-orange-300'*/ }
+        <div> {/*className='h-screen bg-orange-300'*/}
             <div className='child-container'>
-                <h3>{count}</h3>
+                <h3>{state}</h3>
                 <div>
-                    <button onClick={()=> setCount(count - 1)}>-</button>
-                    <button onClick={()=> setCount((pre)=> pre + 1)}>+</button>
+                    <button onClick={()=> dispatch({type: "DECREMENT", playload: 10})}>DECREMENT</button>
+                    <button onClick={() => dispatch({ type: "INCREMENT", playload : 10})}>INCREMENT</button>
                 </div>
             </div>
         </div>
